810d9fd3330137e3847a9499d40f6e67dae94015
C32182119 <952614103@qq.com>
2017-05-11 15:35:07 +0800

    week 5

--- src/main/resources/mysql.sql
+++ src/main/resources/mysql.sql
@@ -1,6 +1,6 @@
 /*
-SQLyog Ultimate v12.09 (64 bit)
-MySQL - 5.7.17-log : Database - wardrobe
+SQLyog Enterprise v12.09 (64 bit)
+MySQL - 5.7.11 : Database - wardrobe
 *********************************************************************
 */
 
@@ -33,14 +33,17 @@ CREATE TABLE `cloth_info` (
   `color` int(11) DEFAULT NULL,
   `size` int(11) DEFAULT NULL,
   `cloth_img` varchar(255) DEFAULT NULL,
+  `is_favorite` int(11) DEFAULT NULL,
   `is_cleaned` int(11) DEFAULT NULL,
   `is_for_sale` int(11) DEFAULT NULL,
   `is_selled` int(11) DEFAULT NULL,
   `others` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
 
 /*Data for the table `cloth_info` */
+
+insert  into `cloth_info`(`id`,`cloth_id`,`user_id`,`cloth_name`,`cloth_price`,`create_time`,`material`,`occasion`,`season`,`color`,`size`,`cloth_img`,`is_favorite`,`is_cleaned`,`is_for_sale`,`is_selled`,`others`) values (1,'cuqjeiztS10','uuqjd3bSB10','11','11','2017-07-31T16:00:00.000Z',4,0,1,2,0,'',0,0,0,0,NULL),(2,'cuqjerYBJ10','uuqjcXThM10','11sa','SS','2000-02-29T16:00:00.000Z',4,0,1,0,3,'',0,0,0,0,NULL),(3,'cuqjesCNd10','uuqjd3bSB10','11111','111','0',0,0,2,0,0,'',0,0,0,0,NULL),(4,'cuqjixLRV10','uuqjd3bSB10','112','212','0',0,0,0,0,0,'',0,0,0,0,NULL),(5,'cuqjixOXN11','uuqjd3bSB10','12','23213','0',0,0,0,0,0,'',0,0,0,0,NULL),(6,'cuqjixRxG12','uuqjd3bSB10','a\'da\'s\'d','sa\'da\'s\'d','0',0,0,0,0,0,'',0,0,0,0,NULL),(7,'cuqjiygro10','uuqjd3bSB10','111','11111','0',0,0,0,0,0,'',0,0,0,0,NULL),(8,'cuqjiyndw11','uuqjd3bSB10','11','1111','0',0,0,0,0,0,'',0,0,0,0,NULL),(9,'cuqjiyV1D10','uuqjd3bSB10','sa\'da\'s','s\'d','0',0,0,0,0,0,'',0,0,0,0,NULL),(10,'cuqjiyZXy11','uuqjd3bSB10','1','1','0',0,0,0,0,0,'',0,0,0,0,NULL),(11,'cuqjiz2f612','uuqjd3bSB10','1','1','0',0,0,0,0,0,'',0,0,0,0,NULL),(12,'cuqjiz43M13','uuqjd3bSB10','1','1','0',0,0,0,0,0,'',0,0,0,0,NULL),(13,'cuqjizxkk10','uuqjd3bSB10','111','111','0',0,0,0,0,0,'',0,0,0,0,NULL),(14,'cuqjizDGD11','uuqjd3bSB10','11','11','0',0,0,0,0,0,'',0,0,0,0,NULL),(15,'cuqjiA4k710','uuqjd3bSB10','1','1','0',0,0,0,0,0,'',0,0,0,0,NULL);
 
 /*Table structure for table `follow` */
 
@@ -98,9 +101,11 @@ CREATE TABLE `user` (
   `avatar_path` varchar(255) DEFAULT NULL,
   `create_time` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 
 /*Data for the table `user` */
+
+insert  into `user`(`id`,`user_id`,`username`,`password`,`avatar_path`,`create_time`) values (1,'uuqjcXThM10','111','11',NULL,'2017-05-10 16:17:18:898'),(2,'uuqjcYmyK11','1111','11',NULL,'2017-05-10 16:19:11:427'),(3,'uuqjcYuST12','11111','11',NULL,'2017-05-10 16:19:43:427'),(4,'uuqjcYAWf13','1','11',NULL,'2017-05-10 16:20:06:699'),(5,'uuqjcZCyg10','2','1',NULL,'2017-05-10 16:24:11:229'),(6,'uuqjd0aM310','22','1',NULL,'2017-05-10 16:26:22:780'),(7,'uuqjd0EBM10','3','3',NULL,'2017-05-10 16:28:17:462'),(8,'uuqjd3bSB10','11','11',NULL,'2017-05-10 16:38:22:013'),(9,'uuqjijZRD10','222','1',NULL,'2017-05-11 14:16:29:081');
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
